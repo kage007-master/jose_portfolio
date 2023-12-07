fac2bb54fe62aec169fd231744bd406ba939ea04
raika-dev <raika.crypto.dev@gmail.com>
2023-12-07 14:02:57 -0500

    change user avatar

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -6,6 +6,7 @@ import MyLink from "./MyLink";
 import Image from "next/image";
 import Slide from "./Slide";
 import Typing from "./Typing";
+import UserAvatar from "../assets/jose.png";
 interface Props {
   profileData: ProfileData;
 }
@@ -18,7 +19,7 @@ export default function ProfileCard({ profileData }: Props) {
 
         <div className="relative z-20 w-56 h-56 mx-auto rounded-full -mt-36 profilePic !border-4 !border-r-lime-300">
           <Image
-            src={profileData.ownersPhoto}
+            src={UserAvatar}
             alt="userPic"
             width="140"
             height="140"
