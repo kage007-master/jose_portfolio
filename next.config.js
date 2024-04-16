fd2983efac07850b449a75884fccc7b12729f23d
Jose <jose10084u@gmail.com>
2024-04-16 10:06:10 -0700

    minor issue fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  unoptimized: true,
   swcMinify: true,
   images: {
     domains: ["media.graphassets.com", "res.cloudinary.com" ,"media.dev.to"],
